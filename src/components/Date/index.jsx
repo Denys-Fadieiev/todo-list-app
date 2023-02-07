c3153719955690a260cec519bda49f7b22c813a2
Denys-Fadieiev <dencheek0911@gmail.com>
2023-02-07 19:58:55 +0200

    correct Date component
    add Date obj in the Date component

--- src/components/Date/index.jsx
+++ src/components/Date/index.jsx
@@ -2,26 +2,22 @@ import React from 'react';
 import styles from './Date.module.scss';
 
 const { wrapperDate, time, date } = styles;
+const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec', ];
 
-const Date = () => {
-  const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec', ];
+const days = [ 'Sanday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', ];
 
-  const days = [ 'Sanday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', ];
-
-  const getTime = (t = new Date()) => {
-    let hours = t.getHours();
-    let minuts = t.getMinuts();
-
-    return `${hours}:${minuts}`
-  };
-
-
+const DateTodoList = () => {
+  const t = new Date();
+  let currentTime = t.getHours()+':'+t.getMinutes();
+  let day = days[t.getDay()];
+  let month = months[t.getMonth()];
+  let currentDate = day+', '+month+' '+t.getDate();
   return (
     <div className={wrapperDate}>
-      <p className={time}>12:38</p>
-      <p className={date}>saturday, jun 20</p>
+      <p className={time}>{currentTime}</p>
+      <p className={date}>{currentDate}</p>
     </div>
   );
 }
 
-export default Date;
+export default DateTodoList;
